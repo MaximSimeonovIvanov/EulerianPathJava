2d502149e9a992ace88ffae9ee120d71dfb1f87b
MaximSimeonovIvanov <maxim.s.ivanoff@gmail.com>
2026-01-15 15:25:27 +0200

    defining the class and main variables

--- src/EulerianPathDirectedEdgesAdjacencyList.java
+++ src/EulerianPathDirectedEdgesAdjacencyList.java
@@ -1,2 +1,19 @@
+//Finding an Eulerian path in a graph
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class EulerianPathDirectedEdgesAdjacencyList {
+    private int edgeCount; //брой ребра
+    private final int n; //брой върхове
+    private int[] in, out; //степени на "вън" и вътре
+    private LinkedList<Integer> path; //резултат
+    private List<List<Integer>> graph; //граф
+
+    //конструктор
+    public EulerianPathDirectedEdgesAdjacencyList(List<List<Integer>> graph){
+        //initialize
+    }
 }
